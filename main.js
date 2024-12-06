cc6bcf32a4f9e6fbf5b82d22bc1d0627b98ed432
daulJsx <mrdaul09@gmail.com>
2024-12-06 17:20:56 +0700

    feat: MPA compatibility

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-export const pageGreeting = (name) => {
+export const pageTitle = (title) => {
   const h1 = document.getElementById("title");
   if (h1) {
-    h1.innerText = `Hello ${name}`;
+    h1.innerText = `${title} Page`;
   } else {
     console.warn("Element with ID 'title' not found");
   }
